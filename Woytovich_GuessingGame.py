b4c49118993312c87c934427bdb8fd15769885e6
wwoytovich <woody@woytovich.com>
2016-02-17 00:00:53 -0500

    guessing game fix
    added win and lose messages

--- Woytovich_GuessingGame.py
+++ Woytovich_GuessingGame.py
@@ -1,5 +1,10 @@
 import random
 answer  = random.randint(0, 500)
-humanInput = input("Enter a number between 0 and 500 ")
+# answer = 45 #needed if you want to try the win condition properly.
+humanInput = input("Enter a number between 0 and 500: ")
+if(humanInput == str(answer)):
+	print("You Win")
+else:
+	print("You lost the game, and you lost the game.")
 print("Your guess was " + humanInput)
 print("The correct number was " + str(answer))
