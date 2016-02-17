40696870aa29db4d22793d3f3eee5bdd74853955
wwoytovich <woody@woytovich.com>
2016-02-17 15:39:46 -0500

    class upload
    uploaded classwork 2/17/2016

--- Woytovich_GuessingGame.py
+++ Woytovich_GuessingGame.py
@@ -1,4 +1,5 @@
 import random
+import binascii
 answer  = random.randint(0, 500)
 # answer = 45 #needed if you want to try the win condition properly.
 humanInput = input("Enter a number between 0 and 500: ")
@@ -7,4 +8,8 @@ if(humanInput == str(answer)):
 else:
 	print("You lost the game, and you lost the game.")
 print("Your guess was " + humanInput)
-print("The correct number was " + str(answer))
+print("The correct number was " + str(answer)
+
+
+
+
